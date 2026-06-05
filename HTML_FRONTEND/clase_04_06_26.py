9c663395576c7708d56283c5c6d16ea36afb4294
Bryan Rivas <161070944+Chinno001@users.noreply.github.com>
2026-06-04 22:18:53 -0400

    04/06/2026 22:20 - Se agregó una imagen del dui al currículum y se actualizó la descripción personal para incluir tecnologías adicionales como Bootstrap.

---
+++ HTML_FRONTEND/clase_04_06_26.py
@@ -0,0 +1,27 @@
+#Clase 2: Estructura de una pagina web en HTML
+
+#Clases de HTML:
+#Se pueden usar para definir atributos en las etiquetas, lo que permite aplicar estilos o comportamientos específicos a los elementos que pertenecen a esa clase.
+#Por ejemplo, si queremos que todos los elementos con la clase "resaltado" tengan un fondo amarillo, podríamos definir esa clase y luego asignar esa clase a los elementos que queremos resaltar con amarillo.
+
+
+#Componentes de un estilo CSS: 
+#1. Selector: Es la etiqueta, clase o id que se utiliza para seleccionar el elemento al que se le quiere aplicar el estilo.
+#2. Propiedad: Es la característica del elemento que se quiere modificar, como el color, el tamaño o la fuente.
+#3. Valor: Es el valor que se le asigna a la propiedad, como "red" para el color o "16px" para el tamaño.
+
+
+#Las propiedades de estilo puedo llamarlas de diferentes maneras, con #, con punto o sin nada, dependiendo de si quiero llamar a una etiqueta, clase o id respectivamente.
+#Una etiqueta se llama sin nada, solamente con el nombre de la etiqueta, como "div" o "p".
+#Una clase se llama con punto (.)
+# y un id se llama con (#).
+
+
+#Bootstrap: Es un framework de CSS que proporciona estilos predefinidos y 
+#componentes para facilitar el diseño de páginas web. 
+#Permite crear sitios web responsivos y atractivos sin necesidad de escribir mucho código CSS desde cero.
+#De alto valor laboral, ya que es ampliamente utilizado en la industria del desarrollo web.
+
+#Botones en HTML: Se pueden crear utilizando la etiqueta <button> o <a> con estilos CSS para darles apariencia de botón.
+#Ejemplo de botón con la etiqueta <button>:
+
