fc64a9d9e55e5e5c1bf30ba105e2fe8afbd9d0b4
Bryan Rivas <161070944+Chinno001@users.noreply.github.com>
2026-06-11 21:48:30 -0400

    Clase 11-06

---
+++ scripts/iniciojs.js
@@ -0,0 +1,21 @@
+//Los comentarios en JavaScript se pueden hacer de dos formas:
+// Comentario de una sola línea
+/* Comentario de varias líneas
+   que puede abarcar varias líneas */
+
+// Variables en JavaScript
+// En Java se define una variable con let o const, dependiendo de si la variable es mutable o inmutable.
+let laprimera = "Hola, Bryan, soy tu primera variable en Javascript!";
+let nombre = "Bryan";
+let edad = 25;
+
+//Constantes en JavaScript
+const laprimeraconstante = "Soy una constante, no puedo cambiar mi valor!";
+const miNombre = "Bryan";
+console.log(laprimeraconstante); /*Con console.log() puedo imprimir el valor de una variable
+                                 en la consola del navegador web*/
+
+let numero1 = 10;
+let numero2 = 15;
+let misuma = numero1 + numero2;
+console.log("La suma de " + numero1 + " y " + numero2 + " es igual a " + misuma); //Con console.log() puedo imprimir el resultado de una operación matemática.
