340e1269291ea376b938653487824cf2c0a6c0ff
Bryan Rivas <161070944+Chinno001@users.noreply.github.com>
2026-06-18 21:21:58 -0400

    21:22

--- FRONTEND_AP_N3_C2_ci2-main/scripts/formulario.js
+++ FRONTEND_AP_N3_C2_ci2-main/scripts/formulario.js
@@ -12,52 +12,66 @@ function validarFormulario(){
     
     if(inputNombre.value == ''){
         inputNombre.classList.add('is-invalid');
+        
     }else{
         inputNombre.classList.remove('is-invalid');
+        inputNombre.classList.add('is-valid');
     };
     if(inputEmail.value == ''){
         inputEmail.classList.add('is-invalid');
+    
     }else{
         inputEmail.classList.remove('is-invalid');
+        inputEmail.classList.remove('is-valid');
     };
     if(inputRut.value == ''){
         inputRut.classList.add('is-invalid');
+    
     }else{
         inputRut.classList.remove('is-invalid');
+        inputRut.classList.remove('is-valid');
     };
     if(inputFechaNacimiento.value == ''){
         inputFechaNacimiento.classList.add('is-invalid');
+
     }else{
         inputFechaNacimiento.classList.remove('is-invalid');
+        inputFechaNacimiento.classList.add('is-valid');
     };
     if(selectGenero.value == ''){
         selectGenero.classList.add('is-invalid');
     }else{
         selectGenero.classList.remove('is-invalid');
+        selectGenero.classList.add('is-valid');
     };
     if(inputDireccion.value == ''){
         inputDireccion.classList.add('is-invalid');
     }else{
         inputDireccion.classList.remove('is-invalid');
+        inputDireccion.classList.add('is-valid');
     };
     if(selectNacionalidad.value == ''){
-        selectNacionalidad.classList.add('alerta');
+        selectNacionalidad.classList.add('is-invalid');
     }else{
-        selectNacionalidad.classList.remove('alerta');
+        selectNacionalidad.classList.remove('is-invalid');
+        selectNacionalidad.classList.add('is-valid');
     };
     if(inputFoto.value == ''){
         inputFoto.classList.add('is-invalid');
     }else{
         inputFoto.classList.remove('is-invalid');
+        inputFoto.classList.add('is-valid');
     };
     if(inputContrasena.value == ''){
         inputContrasena.classList.add('is-invalid');
     }else{
         inputContrasena.classList.remove('is-invalid');
+        inputContrasena.classList.add('is-valid');
     };
     if(inputRepetirContrasena.value == ''){
         inputRepetirContrasena.classList.add('is-invalid');
     }else{
         inputRepetirContrasena.classList.remove('is-invalid');
+        inputRepetirContrasena.classList.add('is-valid');
     };
 };
