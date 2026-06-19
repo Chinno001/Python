dda2a974d42c473f8c41f32836ecab9e3f5fe64b
Bryan Rivas <161070944+Chinno001@users.noreply.github.com>
2026-06-18 22:51:57 -0400

    Fin clase 18-06

--- FRONTEND_AP_N3_C2_ci2-main/scripts/formulario.js
+++ FRONTEND_AP_N3_C2_ci2-main/scripts/formulario.js
@@ -1,3 +1,5 @@
+// mensajeError.style.display = 'none';
+
 function validarFormulario() {
     const inputNombre = document.getElementById('inputNombre');
     const inputEmail = document.getElementById('inputEmail');
@@ -9,26 +11,101 @@ function validarFormulario() {
     const inputFoto = document.getElementById('inputFoto');
     const inputContrasena = document.getElementById('inputContrasena');
     const inputRepetirContrasena = document.getElementById('inputRepetirContrasena');
+    let formularioValido = true;
 
-    validarInput(inputNombre);
-    validarInput(inputEmail);
-    validarInput(inputRut);
-    validarInput(inputFechaNacimiento);
-    validarInput(selectGenero);
-    validarInput(inputDireccion);
-    validarInput(selectNacionalidad);
-    validarInput(inputFoto);
-    validarInput(inputContrasena);
-    validarInput(inputRepetirContrasena);
+    if (!validarInput(inputNombre)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputEmail) || !validarEmail(inputEmail)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputRut)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputFechaNacimiento)) {
+        formularioValido = false;
+    };
+    if (!validarInput(selectGenero)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputDireccion)) {
+        formularioValido = false;
+    };
+    if (!validarInput(selectNacionalidad)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputFoto) || !validarFoto(inputFoto)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputContrasena) || !validarContrasenaSegura(inputContrasena)) {
+        formularioValido = false;
+    };
+    if (!validarInput(inputRepetirContrasena)) {
+        formularioValido = false;
+    };
+
+    if (formularioValido) {
+        alert('Formulario Válido!\nEnviando datos al servidor!')
+    } else {
+        alert('Formulario Inválido!\nRevise los campos en rojo!.')
+    }
 };
 
 function validarInput(input) {
     if (input.value == '') {
+        input.classList.remove('is-valid');
         input.classList.add('is-invalid');
+        return false;
     } else {
         input.classList.remove('is-invalid');
         input.classList.add('is-valid');
+        return true;
     };
+};
+
+function validarEmail(input) {
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (regexEmail.test(input.value)) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        return true;
+    } else {
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        return false;
+    }
+};
+
+function validarContrasenaSegura(input) {
+    const regexContrasenaSegura = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
+    if (regexContrasenaSegura.test(input.value)) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        return true;
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+        return false;
+    }
+};
+
+function validarFoto(input) {
+    const tiposPermitidos = [
+        'image/jpeg',
+        'image/png'
+    ];
+    const archivo = input.files[0];
+    if (archivo) {
+        if (tiposPermitidos.includes(archivo.type)) {
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+            return true;
+        } else {
+            input.classList.remove('is-valid');
+            input.classList.add('is-invalid');
+            return false;
+        }
+    }
 };
 
 function limpiarFormulario() {
